9f24f26f456f14f0d50f32eea9fc8a76a90081f0
Cleyton Silva <silva.rcleyton@gmail.com>
2023-02-18 15:00:07 -0300

    Create hero section

---
+++ javascripts/homeSlide.js
@@ -0,0 +1,9 @@
+var slides = document.querySelectorAll('.slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
